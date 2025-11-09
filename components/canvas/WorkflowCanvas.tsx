70cc615480d9c3d0590c80f3f2c85cedfca91c2e
irpolly <i.r.polly@gmail.com>
2025-11-10 00:30:00 +0200

    ffff
    ff

---
+++ components/canvas/WorkflowCanvas.tsx
@@ -0,0 +1,4 @@
+
+// This file is intentionally left empty.
+// The logic has been moved to pages/BuilderPage.tsx to better utilize ReactFlow's hooks
+// within the provider scope, simplifying state management.
